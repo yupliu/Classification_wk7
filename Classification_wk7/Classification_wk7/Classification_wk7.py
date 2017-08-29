e750170c0438f74edb42f6b173e509af16af5f13
Alan Yu <yupliu@users.noreply.github.com>
2017-08-28 22:57:55 -0400

    Working

--- Classification_wk7/Classification_wk7/Classification_wk7.py
+++ Classification_wk7/Classification_wk7/Classification_wk7.py
@@ -1,8 +1,11 @@
 from __future__ import division
 import graphlab
-products = graphlab.SFrame('D:\\ML_Learning\\UW_Classification\\week7\\amazon_baby_subset.gl\\')
+#products = graphlab.SFrame('D:\\ML_Learning\\UW_Classification\\week7\\amazon_baby_subset.gl\\')
+products = graphlab.SFrame('C:\\Machine_Learning\\Classification_wk7\\amazon_baby_subset.gl\\')
 import json
-with open('D:\\ML_Learning\\UW_Classification\\week7\\important_words.json', 'r') as f: 
+#with open('D:\\ML_Learning\\UW_Classification\\week7\\important_words.json', 'r') as f: 
+with open('C:\\Machine_Learning\\Classification_wk7\\important_words.json', 'r') as f:
+
     important_words = json.load(f)
 important_words = [str(s) for s in important_words]
 
@@ -66,6 +69,105 @@ def compute_avg_log_likelihood(feature_matrix, sentiment, coefficients):
     mask = np.isinf(logexp)
     logexp[mask] = -scores[mask]
     
-    lp = np.sum((indicator-1)*scores - logexp)/len(feature_matrix)
-    
+    lp = np.sum((indicator-1)*scores - logexp)/len(feature_matrix)    
     return lp
+
+j = 1                        # Feature number
+i = 10                       # Data point number
+coefficients = np.zeros(194) # A point w at which we are computing the gradient.
+
+predictions = predict_probability(feature_matrix_train[i:i+1,:], coefficients)
+indicator = (sentiment_train[i:i+1]==+1)
+
+errors = indicator - predictions        
+gradient_single_data_point = feature_derivative(errors, feature_matrix_train[i:i+1,j])
+print "Gradient single data point: %s" % gradient_single_data_point
+print "           --> Should print 0.0"
+
+j = 1                        # Feature number
+i = 10                       # Data point start
+B = 10                       # Mini-batch size
+coefficients = np.zeros(194) # A point w at which we are computing the gradient.
+
+predictions = predict_probability(feature_matrix_train[i:i+B,:], coefficients)
+indicator = (sentiment_train[i:i+B]==+1)
+
+errors = indicator - predictions        
+gradient_mini_batch = feature_derivative(errors, feature_matrix_train[i:i+B,j])
+print "Gradient mini-batch data points: %s" % gradient_mini_batch
+print "                --> Should print 1.0"
+
+from math import sqrt
+def logistic_regression_SG(feature_matrix, sentiment, initial_coefficients, step_size, batch_size, max_iter):
+    log_likelihood_all = []
+    
+    # make sure it's a numpy array
+    coefficients = np.array(initial_coefficients)
+    # set seed=1 to produce consistent results
+    np.random.seed(seed=1)
+    # Shuffle the data before starting
+    permutation = np.random.permutation(len(feature_matrix))
+    feature_matrix = feature_matrix[permutation,:]
+    sentiment = sentiment[permutation]
+    
+    i = 0 # index of current batch
+    # Do a linear scan over data
+    for itr in xrange(max_iter):
+        # Predict P(y_i = +1|x_i,w) using your predict_probability() function
+        # Make sure to slice the i-th row of feature_matrix with [i:i+batch_size,:]
+        ### YOUR CODE HERE
+        predictions = predict_probability(feature_matrix_train[i:i+batch_size,:], coefficients)
+        
+        # Compute indicator value for (y_i = +1)
+        # Make sure to slice the i-th entry with [i:i+batch_size]
+        ### YOUR CODE HERE
+        indicator = (sentiment_train[i:i+batch_size]==+1)
+        
+        # Compute the errors as indicator - predictions
+        errors = indicator - predictions
+        for j in xrange(len(coefficients)): # loop over each coefficient
+            # Recall that feature_matrix[:,j] is the feature column associated with coefficients[j]
+            # Compute the derivative for coefficients[j] and save it to derivative.
+            # Make sure to slice the i-th row of feature_matrix with [i:i+batch_size,j]
+            ### YOUR CODE HERE
+            derivative = feature_derivative(errors, feature_matrix_train[i:i+batch_size,j])
+            
+            # compute the product of the step size, the derivative, and the **normalization constant** (1./batch_size)
+            ### YOUR CODE HERE
+            coefficients[j] += step_size * derivative
+        
+        # Checking whether log likelihood is increasing
+        # Print the log likelihood over the *current batch*
+        lp = compute_avg_log_likelihood(feature_matrix[i:i+batch_size,:], sentiment[i:i+batch_size],
+                                        coefficients)
+        log_likelihood_all.append(lp)
+        if itr <= 15 or (itr <= 1000 and itr % 100 == 0) or (itr <= 10000 and itr % 1000 == 0) or itr % 10000 == 0 or itr == max_iter-1:
+            data_size = len(feature_matrix)
+            print 'Iteration %*d: Average log likelihood (of data points in batch [%0*d:%0*d]) = %.8f' % (int(np.ceil(np.log10(max_iter))), itr,int(np.ceil(np.log10(data_size))), i,int(np.ceil(np.log10(data_size))), i+batch_size, lp)
+        
+        # if we made a complete pass over data, shuffle and restart
+        i += batch_size
+        if i+batch_size > len(feature_matrix):
+            permutation = np.random.permutation(len(feature_matrix))
+            feature_matrix = feature_matrix[permutation,:]
+            sentiment = sentiment[permutation]
+            i = 0
+                
+    # We return the list of log likelihoods for plotting purposes.
+    return coefficients, log_likelihood_all
+
+sample_feature_matrix = np.array([[1.,2.,-1.], [1.,0.,1.]])
+sample_sentiment = np.array([+1, -1])
+
+coefficients, log_likelihood = logistic_regression_SG(sample_feature_matrix, sample_sentiment, np.zeros(3),
+                                                  step_size=1., batch_size=2, max_iter=2)
+print '-------------------------------------------------------------------------------------'
+print 'Coefficients learned                 :', coefficients
+print 'Average log likelihood per-iteration :', log_likelihood
+if np.allclose(coefficients, np.array([-0.09755757,  0.68242552, -0.7799831]), atol=1e-3)\
+  and np.allclose(log_likelihood, np.array([-0.33774513108142956, -0.2345530939410341])):
+    # pass if elements match within 1e-3
+    print '-------------------------------------------------------------------------------------'
+    print 'Test passed!'
+else:
+    print '-------------------------------------------------------------------------------------'
